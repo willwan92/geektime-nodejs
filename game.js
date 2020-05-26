7b1fc2c6e9e4e3d2c89eafdea934fa2ea6da2ee9
Will Wan <wilson3cy@163.com>
2020-05-26 11:13:12 +0800

    feat: node.js 模块

---
+++ game.js
@@ -0,0 +1,32 @@
+
+function game(playerAction) {
+	var random = Math.random() * 3;
+	var computerAction = '石头';
+
+	if (random > 2) {
+		computerAction = '布';
+	} else if (random < 1) {
+		computerAction = '剪刀';
+	}
+
+	console.log('电脑：' + computerAction);
+	console.log('你：' + playerAction);
+
+	if (playerAction === computerAction) {
+		console.log("平局");
+		return 0;
+	} else if (
+		(playerAction === '剪刀' && computerAction === '布') ||
+		(playerAction === '布' && computerAction === '石头') ||
+		(playerAction === '石头' && computerAction === '剪刀')
+	) {
+		console.log('恭喜，你赢了');
+		return 1;
+	} else {
+		console.log('真遗憾，你输了');
+		return -1;
+	}
+}
+
+exports.game = game;
+
--- index.js
+++ index.js
@@ -1,28 +1,23 @@
+const { game } = require('./game');
+
 // process.argv 获取nodejs进程中的参数
-var playerAction = process.argv[2];
+// var playerAction = process.argv[2];
 
-var random = Math.random() * 3;
-var computerAction = '石头';
+let count = 0;
 
-if (random > 2) {
-    computerAction = '布';
-} else if (random < 1) {
-    computerAction = '剪刀';
-}
+process.stdin.on('data', e => {
+	const playerAction = e.toString().trim();
+	const result = game(playerAction);
 
-console.log('电脑：' + computerAction);
-console.log('你：' + playerAction);
+	if (result === 1) {
+		count ++;
+	}
 
-if (playerAction === computerAction) {
-    console.log("平局");
+	if (count === 3) {
+		console.log('你太厉害了，我不玩了！');
+		// 杀掉进程
+		process.exit();
+	}	
+})
 
-} else if (
-    (playerAction === '剪刀' && computerAction === '布') ||
-    (playerAction === '布' && computerAction === '石头') ||
-    (playerAction === '石头' && computerAction === '剪刀')
-) {
-    console.log('恭喜，你赢了');
 
-} else {
-    console.log('真遗憾，你输了');
-}
